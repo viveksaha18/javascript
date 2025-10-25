c6b28003fa666aed688fd950941c8c19f6d79c30
Vivek18 <viveksaha096@gmail.com>
2025-10-25 08:17:27 +0530

    Basic Loops Added!

---
+++ Basics/loops.js
@@ -0,0 +1,16 @@
+// for loop
+for(let i = 0; i < 5; i++) {
+    console.log("For Loop");
+}
+// while loop
+let i = 0;
+while(i < 5) {
+    console.log("While Loop"); 
+    i++;
+}
+// do while
+let y = 0;
+do {
+    console.log("Even If the condition is wrong it will print once");
+    y++;
+}while(y>5);
