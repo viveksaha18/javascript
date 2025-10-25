896efbbd68abf28b1f4c3fd41dc4ff0e189a7824
Vivek18 <viveksaha096@gmail.com>
2025-10-25 08:12:36 +0530

    logical Operators

---
+++ Basics/logicalOperators.js
@@ -0,0 +1,13 @@
+// && if both condition is true then true
+
+if(5 > 3 && 6 > 2) {
+    console.log("AND OPERATOR both the condition are true");
+}
+// || if only one condition true
+if(5 > 3 || 2 > 5) {
+    console.log("OR OPERATOR only one condition are true");
+}
+
+// Logical NOT Operator !
+let age = !(5>3);
+console.log(age);
