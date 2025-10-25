54b2df5294ebc10d86a633d99c66d57f0aa15588
Vivek18 <viveksaha096@gmail.com>
2025-10-25 08:23:15 +0530

    Object Creation Done!

---
+++ Basics/objectCreation.js
@@ -0,0 +1,13 @@
+// Object in Javascript is a collection of key value pair
+
+let students = {
+    name : "Vivek",
+    age : "21",
+}
+console.log(students.name);
+
+let car = new Object();
+car.brand = 'Jaguar';
+car.year = 2025;
+
+console.log(car.brand);
