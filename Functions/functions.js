e24944f173eab75b88a57f846faf61aedd64c26c
Vivek18 <viveksaha096@gmail.com>
2025-10-26 10:24:57 +0530

    Updated

--- Functions/functions.js
+++ Functions/functions.js
@@ -16,3 +16,17 @@ let y = function multiply(n1, n2) {
 }
 let z = y(2, 2);
 console.log(z)
+
+
+// argument keyword
+function addition() {
+    // arguments -> Array 
+    let sum = 0;
+    for(let i = 0; i < arguments.length; i++) {
+        sum +=  arguments[i];
+    }
+    return sum;
+}
+
+let sum = addition(10,20,30,50);
+console.log(sum)
