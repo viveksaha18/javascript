c0c969bee96b4cd07c61e8201c41df09874c5c5a
Vivek18 <viveksaha096@gmail.com>
2025-10-27 22:56:19 +0530

    Arrays Added!

---
+++ Arrays/creation.js
@@ -0,0 +1,11 @@
+// Syntax for declaring the array
+let arr = ["Vivek", "Saha", "John"];
+console.log(arr)
+// Array in Javascript is heterogenous like doesnot contain similar types of data 
+let heterogenous = ["Vivek", 1, true];
+console.log(heterogenous)
+// Some built in function 
+arr.push("Virat");
+console.log(arr);
+heterogenous.pop();
+console.log(heterogenous)
