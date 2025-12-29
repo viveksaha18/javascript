df5537964b994017a67e1999ad4d46ab6aa63c48
Vivek18 <viveksaha096@gmail.com>
2025-12-29 22:50:48 +0530

    Literals In Js

---
+++ Basics/literals.js
@@ -0,0 +1,10 @@
+// Template Literals in JavaScript : Using backticks (`) to create strings and embed expressions. 
+//  In Short : variables inside strings.
+
+let city = "London";
+
+console.log(`The weather in ${city} is great!`);
+
+let a = 5;
+let b = 10;
+console.log(`sum: ${a + b}`);
