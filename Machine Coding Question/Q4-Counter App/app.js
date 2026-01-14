ad4040a10a2957d1d1544829d311d27df4efc11d
Vivek18 <viveksaha096@gmail.com>
2026-01-14 12:11:18 +0530

    Going with Counter Application

---
+++ Machine Coding Question/Q4-Counter App/app.js
@@ -0,0 +1,9 @@
+console.log('I am running.....')
+const add = document.getElementById('add');
+const subtract = document.getElementById('subtract');
+const reset = document.getElementById('reset');
+const initailValue = document.getElementById('initial');
+console.log(initailValue.value);
+add.addEventListener('click', ()=>{
+    
+})
