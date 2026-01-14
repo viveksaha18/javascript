b79795e38987a5d2c4895a1072ff463e0f7500f3
Vivek18 <viveksaha096@gmail.com>
2026-01-14 12:45:54 +0530

    Done with Counter Application

--- Machine Coding Question/Q4-Counter App/app.js
+++ Machine Coding Question/Q4-Counter App/app.js
@@ -3,7 +3,25 @@ const add = document.getElementById('add');
 const subtract = document.getElementById('subtract');
 const reset = document.getElementById('reset');
 const initailValue = document.getElementById('initial');
-console.log(initailValue.value);
+console.log(initailValue.textContent);
+const num = initailValue.textContent;
+console.log(typeof(num))
+let value = parseInt(num);
 add.addEventListener('click', ()=>{
-    
+    value +=1;
+    initailValue.textContent = value;
+})
+subtract.addEventListener('click', ()=>{
+    if(value>0) {
+        value-=1;
+        initailValue.textContent = value;
+    }
+    else {
+        alert('Value cannot be minus');
+    }
+})
+
+reset.addEventListener('click', () => {
+    value = 0;
+    initailValue.textContent = value;
 })
