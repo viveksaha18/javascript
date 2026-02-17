0bdcf2d8387b73de0770cf9c63afa4b82daa6a9e
Vivek18 <viveksaha096@gmail.com>
2026-02-17 20:49:14 +0530

    Understanding closures

--- Closures/closures.js
+++ Closures/closures.js
@@ -10,4 +10,11 @@ function parentFunction() {
 }
 parentFunction()
 
+// How it is working give step by step explanation:
+// 1. When parentFunction is called, it creates a local variable name and assigns it the value 'Parent Function'.
+// 2. Then, it defines the childFunction, which is a nested function inside parentFunction.
+// 3. The childFunction has access to the variables in its outer scope, which includes the name variable defined in parentFunction.
+// 4. When childFunction is called, it logs the value of name to the console, which is 'Parent Function'.
+// 5. Finally, parentFunction is called to execute the code and see the result.
+
 // In this above code you see how the name variable of parentFunction is accessed by the childFunction, this describes how it access the out of the scope which is lexical scoping.
